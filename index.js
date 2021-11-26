3c8770749a394eb4b1d7161f7039f706b5f606bc
simon mok <simon.mok@oocl.com>
2021-11-26 19:26:53 +0800

    add ans

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+console.log(Array.isArray(a));
+console.log(Array.isArray(b));
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+for(i = 0 ; i < a.length ; i++)
+{
+	a[i] = a[i] *2;
+}
+console.log(a);
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +21,46 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+console.log(colors.join(' '));
+console.log(colors.join('+'));
+console.log(colors.join(','));
+
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+console.log(a.sort(function(in1,in2){return in1-in2;}));
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+function mode(inArr)
+{
+	var count = {};
+	var maxCount = 0;
+	var maxValue = inArr[0];
+
+	for(var i of inArr)
+	{
+		if(count[i])
+		{
+			count[i]++;
+			if(count[i] > maxCount)
+			{
+				maxCount = count[i];
+				maxValue = i;
+			}
+		}
+		else
+		{
+			count[i] = 1;
+
+		}
+	}
+
+
+	return maxValue;
+}
+console.log(mode(a))
